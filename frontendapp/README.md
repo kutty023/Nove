4584cd85059436b716a8cdd91483040f3daae8c0
kutty023 <arthiaaru23@gmail.com>
2024-07-20 10:16:26 +0530

    create frontend app using react and made the required changes

---
+++ frontendapp/README.md
@@ -0,0 +1,5 @@
+## Hey there!!
+
+I'm using React to create frontend for my project
+
+[Frontend Deployed link!]()
---
+++ frontendapp/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='messageContainer'>
+      <h1>Hellooooo</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
